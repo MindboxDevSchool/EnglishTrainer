41a6adc13afa85fbf7a9f14d813082bf9af13348
Nikolay Vorotnev <vrtnev@tltdev.ru>
2020-09-16 13:55:52 +0300

    Initial commit

---
+++ vorotnev/EnglishTrainer/EnglishTrainer/EnglishTrainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EnglishTrainer
+{
+    public class EnglishTrainer
+    {
+        public static void Input()
+        {
+            
+        }
+
+        public static void Solve()
+        {
+            
+        }
+
+        public static void Output()
+        {
+            
+        }
+    }
+
+}
