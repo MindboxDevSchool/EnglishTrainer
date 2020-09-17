b04300a1aa56129de1293b244b573b6aac95090b
iliazen <iliazenin.msu@gmail.com>
2020-09-17 23:11:36 +0300

    Zenin's solution

---
+++ zenin/EnglishTrainer/EnglishTrainer.Tests/ManualTranslationTaskUnitTests.cs
@@ -0,0 +1,70 @@
+﻿using System.Data;
+using NUnit.Framework;
+
+namespace EnglishTrainer.Tests
+{
+    public class ManualTranslationTaskUnitTests
+    {
+        [Test]
+        public void SetOfTwoWordsAndTranslations_CheckAnswers_OneCorrectTranslation()
+        {
+            var task = new ManualTranslationTask();
+            var setOfWords = Vocabulary.CreateEmptyDataTable();
+            setOfWords.Rows.Add("one", "один");
+            setOfWords.Rows.Add("two", "два");
+            string[] answers = {"один", "три"};
+
+            var correctTranslations = task.CheckAnswers(setOfWords, answers);
+            
+            Assert.AreEqual(new string[] {"one"}, correctTranslations);
+        }
+
+        [Test]
+        public void VocabularyFromFileWithWords_CountNonRepeatedWordsVocabulary_AllWordsInVocabularyNonRepeated()
+        {
+            var vocabulary = new Vocabulary("words.csv");
+            var task = new ManualTranslationTask();
+
+            var countOfNonRepeatedWords = task.CountNonRepeatedWordsVocabulary(vocabulary);
+            
+            Assert.AreEqual(vocabulary.Length, countOfNonRepeatedWords);
+        }
+
+        [Test]
+        public void VocabularyFromFileWithWords_CheckIsEnoughWordsInVocabulary_Enough()
+        {
+            var vocabulary = new Vocabulary("words.csv");
+            var task = new ManualTranslationTask();
+
+            var isEnoughWords = task.CheckIsEnoughWordsInVocabulary(vocabulary);
+            
+            Assert.AreEqual(true, isEnoughWords);
+        }
+        
+        [Test]
+        public void VocabularyFromFileWithWords_CountNonRepeatedWordsVocabulary_AllWordsInVocabularyRepeated()
+        {
+            var vocabulary = new Vocabulary("words.csv");
+            var task = new ManualTranslationTask();
+            for (int i = 0; i < vocabulary.Length; i++)
+                vocabulary.Words.Rows[i].SetField("repeated", 4);
+
+            var countOfNonRepeatedWords = task.CountNonRepeatedWordsVocabulary(vocabulary);
+            
+            Assert.AreEqual(0, countOfNonRepeatedWords);
+        }
+        
+        [Test]
+        public void VocabularyFromFileWithWords_CheckIsEnoughWordsInVocabulary_NotEnough()
+        {
+            var vocabulary = new Vocabulary("words.csv");
+            var task = new ManualTranslationTask();
+            for (int i = 0; i < vocabulary.Length; i++)
+                vocabulary.Words.Rows[i].SetField("repeated", 4);
+
+            var isEnoughWords = task.CheckIsEnoughWordsInVocabulary(vocabulary);
+            
+            Assert.AreEqual(false, isEnoughWords);
+        }
+    }
+}
---
+++ zenin/EnglishTrainer/EnglishTrainer/ManualTranslationTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace EnglishTrainer
+{
+    public class ManualTranslationTask : Task
+    {
+        public ManualTranslationTask() : base(10, "russian") {}
+
+        public  override DataTable CreateSetOfWords(Vocabulary vocabulary)
+        {
+            var rand = new Random();
+            var set = Vocabulary.CreateEmptyDataTable();
+
+            if (CheckIsEnoughWordsInVocabulary(vocabulary))
+            {
+                while (set.Rows.Count < WordsInTask)
+                {
+                    var row = vocabulary.Words.Rows[rand.Next(vocabulary.Length)];
+                    if ((int) row["repeated"] < 3 &&
+                        Trainer.FindIndexOfStringInColumn(set, row["english"].ToString(), "english") != -1)
+                        set.Rows.Add(row["english"], row["russian"]);
+                }
+            }
+
+            return set;
+        }
+    }
+}
