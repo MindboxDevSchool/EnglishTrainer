cf36f3c431f097fc5b37a4b959f0a4a4dfb8cce9
Ilya <ilyaaxenov1999@gmail.com>
2020-09-17 14:09:06 +0300

    DataServiceTests added DataService refactored

---
+++ aksenov/EnglishTrainer.Tests/DataServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+
+namespace EnglishTrainer.Tests
+{
+    public class DataServiceTests
+    {
+        [Test]
+        public void LoadWordsFromFile_3WordsInFile_CorrectReading()
+        {
+            //arrange
+            string pathToCSVTestFile = new DirectoryInfo(Directory.GetCurrentDirectory())?.Parent?.Parent?.Parent?.FullName + "\\test files\\DataServiceTestData.csv";
+            
+            DataService dataService = new DataService();
+
+            //act
+            List<VocabularyWord> words = dataService.LoadWordsFromFile(pathToCSVTestFile);
+
+            //assert
+            Assert.AreEqual("red", words[1].Spelling);
+            Assert.AreEqual("красный", words[1].Translation);
+        }
+    }
+}
--- aksenov/EnglishTrainer/DataService.cs
+++ aksenov/EnglishTrainer/DataService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 namespace EnglishTrainer
 {
@@ -10,12 +11,15 @@ namespace EnglishTrainer
         {
             List<VocabularyWord> loadedWords = new List<VocabularyWord>();
 
-            using (StreamReader reader = new StreamReader(new FileStream(path, FileMode.Open)))
+            using (StreamReader reader = new StreamReader(new FileStream(path, FileMode.Open), Encoding.Default))
             {
-                string[] line = reader.ReadLine()?.Split(";");
+                while (!reader.EndOfStream)
+                {
+                    string[] line = reader.ReadLine()?.Split(";");
 
-                if (line != null)
-                    loadedWords.Add(new VocabularyWord(line[0], line[1]));
+                    if (line != null && line.Length >= 2)
+                        loadedWords.Add(new VocabularyWord(line[0], line[1]));
+                }
             }
             
             if (loadedWords.Count == 0)
