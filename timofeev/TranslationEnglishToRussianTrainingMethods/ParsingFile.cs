53f3d16e15fac0de022bbdc312658d199be05d1d
Giralis <vladimirmail895@gmail.com>
2020-09-18 15:54:11 +0300

    initial commit

---
+++ timofeev/TranslationEnglishToRussianTrainingMethods/ParsingFile.cs
@@ -0,0 +1,18 @@
+﻿namespace TranslationEnglishToRussianTrainingMethods
+{
+    public class Parsing
+    {
+        public string EnglishWord { get; set; }
+        public string RussianWord { get; set; }
+
+        public static Parsing ParseRow(string row)
+        {
+            var columns = row.Split(';');
+            return new Parsing()
+            {
+                EnglishWord = columns[0],
+                RussianWord = columns[1]
+            };
+        }
+    }
+}
